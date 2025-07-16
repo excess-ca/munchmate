9e663262ceed8a1b92b6413e3b9c446ce270217c
excess-ca <150656390+excess-ca@users.noreply.github.com>
2025-07-16 18:48:24 +0800

    + added magic card to test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { MagicCard } from "@/components/magicui/magic-card";
 
 export default function Home() {
   return (
     <main>
+      <MagicCard gradientColor="bg-gradient-to-r from-purple-500 to-pink-500" className="rounded-2xl max-w-auto" >
+        <div className="p-4">
+          <p>Hello World</p>
+          <span>Hover me</span>
+        </div>
+      </MagicCard>
     </main>
   )
 }
